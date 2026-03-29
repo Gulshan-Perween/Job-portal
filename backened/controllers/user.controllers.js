ddf4fa189149fedb477d675b50d471f626d2b759
Gulshan-Perween <perweengulshan56@gmail.com>
2026-03-29 21:39:57 +0530

    fixs

--- backened/controllers/user.controllers.js
+++ backened/controllers/user.controllers.js
@@ -91,7 +91,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000,
-        sameSite: "strict",
+        sameSite: "none",
+        secure: true,
       })
       .json({
         success: true,
