3769d3466e1603726eebfd862db8e5a3e47b68c6
Gulshan-Perween <perweengulshan56@gmail.com>
2026-03-29 16:38:15 +0530

    fix: update API endpoints to Render URL

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -2,4 +2,4 @@
 export const USER_API_ENDPOINT = "https://job-portal-d30c.onrender.com/api/v1/user";
 export const JOB_API_END_POINT = "https://job-portal-d30c.onrender.com/api/v1/job";
 export const APPLICATION_API_ENDPOINT = "https://job-portal-d30c.onrender.com/api/v1/application";
-export const COMPANY_API_ENDPOINT = "https://job-portal-d30c.onrender.com/api/v1/company";
+export const COMPANY_API_END_POINT = "https://job-portal-d30c.onrender.com/api/v1/company";
