c86c634186b2e0d832b2697a499f90d3c01b033f
Gulshan-Perween <perweengulshan56@gmail.com>
2026-03-29 16:29:54 +0530

    fix: cors origin add vercel url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,5 @@
-export const USER_API_ENDPOINT = "http://localhost:3000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:3000/api/v1/job";
-export const APPLICATION_API_ENDPOINT = "http://localhost:3000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:3000/api/v1/company";
+// ✅ Render URL use karo
+export const USER_API_ENDPOINT = "https://job-portal-d30c.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://job-portal-d30c.onrender.com/api/v1/job";
+export const APPLICATION_API_ENDPOINT = "https://job-portal-d30c.onrender.com/api/v1/application";
+export const COMPANY_API_ENDPOINT = "https://job-portal-d30c.onrender.com/api/v1/company";
